c700bb02a2d8c68e3c34c6912a58798d37e32022
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-04 02:20:14 +0300

    random

--- example003_Summ/Program.cs
+++ example003_Summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 40;
-int numberB = 35;
+﻿int numberA = new Random().Next(1, 100); // 1 2 3 ... 100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); // 1 2 3 ... 9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
